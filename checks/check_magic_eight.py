5351ae4d8311f8b5fb43f289d182755d30feb593
Larry Shatzer, Jr <lshatzer@myriad.com>
2022-02-09 08:15:40 -0700

    Magic 8 checks. (#2)

---
+++ checks/check_magic_eight.py
@@ -0,0 +1,157 @@
+import splunk_appinspect
+import os
+import regex as re
+from splunk_appinspect.configuration_file import ConfigurationFile
+from splunk_appinspect.splunk import normalizeBoolean
+
+
+@splunk_appinspect.tags("best_practices", "best_practices_magic_eight")
+@splunk_appinspect.cert_version(min="2.14.1")
+def check_should_linemerge(app, reporter):
+    """Check that SHOULD_LINEMERGE is set to false"""
+    property = "SHOULD_LINEMERGE"
+    config_file_paths = app.get_config_file_paths("props.conf")
+    for directory, filename in iter(config_file_paths.items()):
+        file_path = os.path.join(directory, filename)
+        props_config: ConfigurationFile = app.props_conf(directory)
+        for stanza in props_config.sections():
+            if not stanza.has_option(property):
+                output = f"{property} is not set for [{stanza.name}]"
+                reporter.warn(output, file_path, stanza.lineno)
+            elif normalizeBoolean(stanza.get_option(property).value):
+                output = f"{property} is true, when it should be false for [{stanza.name}]"
+                reporter.warn(output, file_path, stanza.lineno)
+
+
+@splunk_appinspect.tags("best_practices", "best_practices_magic_eight")
+@splunk_appinspect.cert_version(min="2.14.1")
+def check_line_breaker(app, reporter):
+    """Check that LINE_BREAKER is set"""
+    property = "LINE_BREAKER"
+    config_file_paths = app.get_config_file_paths("props.conf")
+    for directory, filename in iter(config_file_paths.items()):
+        file_path = os.path.join(directory, filename)
+        props_config: ConfigurationFile = app.props_conf(directory)
+        for stanza in props_config.sections():
+            if not stanza.has_option(property):
+                output = f"{property} is not set for [{stanza.name}]"
+                reporter.warn(output, file_path, stanza.lineno)
+
+
+@splunk_appinspect.tags("best_practices", "best_practices_magic_eight")
+@splunk_appinspect.cert_version(min="2.14.1")
+def check_time_prefix(app, reporter):
+    """Check that TIME_PREFIX is set"""
+    property = "TIME_PREFIX"
+    config_file_paths = app.get_config_file_paths("props.conf")
+    for directory, filename in iter(config_file_paths.items()):
+        file_path = os.path.join(directory, filename)
+        props_config: ConfigurationFile = app.props_conf(directory)
+        for stanza in props_config.sections():
+            if not stanza.has_option(property):
+                output = f"{property} is not set for [{stanza.name}]"
+                reporter.warn(output, file_path, stanza.lineno)
+
+
+@splunk_appinspect.tags("best_practices", "best_practices_magic_eight")
+@splunk_appinspect.cert_version(min="2.14.1")
+def check_max_timestamp_lookahead(app, reporter):
+    """Check that MAX_TIMESTAMP_LOOKAHEAD is set, numeric and >= 0"""
+    property = "MAX_TIMESTAMP_LOOKAHEAD"
+    config_file_paths = app.get_config_file_paths("props.conf")
+    for directory, filename in iter(config_file_paths.items()):
+        file_path = os.path.join(directory, filename)
+        props_config: ConfigurationFile = app.props_conf(directory)
+        for stanza in props_config.sections():
+            if not stanza.has_option(property):
+                output = f"{property} is not set for [{stanza.name}]"
+                reporter.warn(output, file_path, stanza.lineno)
+            else:
+                setting = stanza.get_option(property)
+                if not _is_numeric(setting.value):
+                    output = f"{property} is not numeric for [{stanza.name}] ({setting.value})"
+                    reporter.warn(output, file_path, setting.lineno)
+                elif not int(setting.value) >= 0:
+                    output = f"{property} is not >= 0 [{stanza.name}] ({setting.value})"
+                    reporter.warn(output, file_path, setting.lineno)
+
+
+@splunk_appinspect.tags("best_practices", "best_practices_magic_eight")
+@splunk_appinspect.cert_version(min="2.14.1")
+def check_time_format(app, reporter):
+    """Check that TIME_FORMAT is set"""
+    property = "TIME_FORMAT"
+    config_file_paths = app.get_config_file_paths("props.conf")
+    for directory, filename in iter(config_file_paths.items()):
+        file_path = os.path.join(directory, filename)
+        props_config: ConfigurationFile = app.props_conf(directory)
+        for stanza in props_config.sections():
+            if not stanza.has_option(property):
+                output = f"{property} is not set for [{stanza.name}]"
+                reporter.warn(output, file_path, stanza.lineno)
+
+
+@splunk_appinspect.tags("best_practices", "best_practices_magic_eight")
+@splunk_appinspect.cert_version(min="2.14.1")
+def check_truncate(app, reporter):
+    """Check that TRUNCATE is set"""
+    property = "TRUNCATE"
+    config_file_paths = app.get_config_file_paths("props.conf")
+    for directory, filename in iter(config_file_paths.items()):
+        file_path = os.path.join(directory, filename)
+        props_config: ConfigurationFile = app.props_conf(directory)
+        for stanza in props_config.sections():
+            if not stanza.has_option(property):
+                output = f"{property} is not set for [{stanza.name}]"
+                reporter.warn(output, file_path, stanza.lineno)
+            else:
+                setting = stanza.get_option(property)
+                if not _is_numeric(setting.value):
+                    output = f"{property} is not numeric for [{stanza.name}] ({setting.value})"
+                    reporter.warn(output, file_path, setting.lineno)
+                elif not int(setting.value) > 0:
+                    output = f"{property} is not > 0 [{stanza.name}] ({setting.value})"
+                    reporter.warn(output, file_path, setting.lineno)
+
+
+@splunk_appinspect.tags("best_practices", "best_practices_magic_eight")
+@splunk_appinspect.cert_version(min="2.14.1")
+def check_event_breaker_enable(app, reporter):
+    """Check that EVENT_BREAKER_ENABLE is set to true"""
+    property = "EVENT_BREAKER_ENABLE"
+    config_file_paths = app.get_config_file_paths("props.conf")
+    for directory, filename in iter(config_file_paths.items()):
+        file_path = os.path.join(directory, filename)
+        props_config: ConfigurationFile = app.props_conf(directory)
+        for stanza in props_config.sections():
+            if not stanza.has_option(property):
+                output = f"{property} is not set for [{stanza.name}]"
+                reporter.warn(output, file_path, stanza.lineno)
+            elif not normalizeBoolean(stanza.get_option(property).value):
+                output = f"{property} is false, when it should be true for [{stanza.name}]"
+                reporter.warn(output, file_path, stanza.lineno)
+
+
+@splunk_appinspect.tags("best_practices", "best_practices_magic_eight")
+@splunk_appinspect.cert_version(min="2.14.1")
+def check_event_breaker(app, reporter):
+    """Check that EVENT_BREAKER is set"""
+    property = "EVENT_BREAKER"
+    config_file_paths = app.get_config_file_paths("props.conf")
+    for directory, filename in iter(config_file_paths.items()):
+        file_path = os.path.join(directory, filename)
+        props_config: ConfigurationFile = app.props_conf(directory)
+        for stanza in props_config.sections():
+            if not stanza.has_option(property):
+                output = f"{property} is not set for [{stanza.name}]"
+                reporter.warn(output, file_path, stanza.lineno)
+
+# Helpers
+
+
+def _is_numeric(property_value):
+    try:
+        int(property_value)
+        return True
+    except ValueError:
+        return False
--- tests/tests.py
+++ tests/tests.py
@@ -4,22 +4,49 @@ from unittest.mock import call
 
 import os
 import sys
+import json
 from splunk_appinspect.app import App
 from splunk_appinspect.python_analyzer.trustedlibs.trusted_libs_manager import TrustedLibsManager
 
 test_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(test_path, ".."))
 
-# Some of these tests need more test cases
 
-
-class TestCheckRegularExpressions(unittest.TestCase):
+class BaseTest(unittest.TestCase):
+    """
+    Base Class for all tests, so we can inherit some utility methods and common setup.
+    """
 
     def setUp(self):
         self.maxDiff = None
+        self.reporter = Mock()
 
     def get_app(self, location):
         return App(location=os.path.join(test_path, location), trusted_libs_manager=TrustedLibsManager())
+
+    def assert_clean(self):
+        self.reporter.warn.assert_not_called()
+        self.reporter.fail.assert_not_called()
+
+    def assert_mocked_calls(self, location):
+        expected_file = os.path.join(test_path, location, "expected.json")
+        if os.path.exists(expected_file):
+            with open(expected_file, "r") as fh:
+                expected_json = json.load(fh)
+                expected_json.sort()
+                calls = json.loads(json.dumps(self.reporter.mock_calls))
+                calls.sort()
+                self.assertListEqual(expected_json, calls)
+        else:
+            self.fail(f"Unable to load file {expected_file}")
+
+
+class TestCheckRegularExpressions(BaseTest):
+    """
+    Tests for check_regular_expression checks.
+    """
+
+    # Some of these tests need more test cases
 
     def test_clean(self):
         """
@@ -29,22 +56,20 @@ class TestCheckRegularExpressions(unittest.TestCase):
         """
         from checks import check_regular_expressions
         app = self.get_app("test_data/check_regular_expressions_clean")
-        reporter = Mock()
         for check_name in [c for c in dir(check_regular_expressions) if c.startswith("check_")]:
-            getattr(check_regular_expressions, check_name)(app, reporter)
-        reporter.warn.assert_not_called()
-        reporter.fail.assert_not_called()
+            getattr(check_regular_expressions, check_name)(app, self.reporter)
+        self.assert_clean()
 
     def test_empty(self):
         """
+        This check empty is just checking an empty app does not throw any
+        failures or errors.
         """
         from checks import check_regular_expressions
         app = self.get_app("test_data/check_regular_expressions_empty")
-        reporter = Mock()
         for check_name in [c for c in dir(check_regular_expressions) if c.startswith("check_")]:
-            getattr(check_regular_expressions, check_name)(app, reporter)
-        reporter.warn.assert_not_called()
-        reporter.fail.assert_not_called()
+            getattr(check_regular_expressions, check_name)(app, self.reporter)
+        self.assert_clean()
 
     def test_check_dynamic_field_names(self):
         """
@@ -53,68 +78,21 @@ class TestCheckRegularExpressions(unittest.TestCase):
         """
         from checks.check_regular_expressions import check_dynamic_field_names_transforms
         from checks.check_regular_expressions import check_dynamic_field_names_props
-        app = self.get_app(
-            "test_data/check_regular_expressions_dynamic_field_names")
-        reporter = Mock()
-        check_dynamic_field_names_transforms(app, reporter)
-        check_dynamic_field_names_props(app, reporter)
-        expected = [call.fail('Have _KEY_1, could not find _VAL_1', 'default/props.conf', 2),
-                    call.fail('Have _KEY_1, could not find _VAL_1',
-                              'default/transforms.conf', 2),
-                    call.fail('Have _KEY_2, could not find _VAL_2',
-                              'default/props.conf', 4),
-                    call.fail('Have _KEY_2, could not find _VAL_2',
-                              'default/props.conf', 5),
-                    call.fail('Have _KEY_2, could not find _VAL_2',
-                              'default/props.conf', 6),
-                    call.fail('Have _KEY_2, could not find _VAL_2',
-                              'default/transforms.conf', 8),
-                    call.fail('Have _KEY_2, could not find _VAL_2',
-                              'default/transforms.conf', 11),
-                    call.fail('Have _KEY_2, could not find _VAL_2',
-                              'default/transforms.conf', 14),
-                    call.fail('Have _VAL_1, could not find _KEY_1',
-                              'default/props.conf', 3),
-                    call.fail('Have _VAL_1, could not find _KEY_1',
-                              'default/props.conf', 4),
-                    call.fail('Have _VAL_1, could not find _KEY_1',
-                              'default/props.conf', 5),
-                    call.fail('Have _VAL_1, could not find _KEY_1',
-                              'default/props.conf', 6),
-                    call.fail('Have _VAL_1, could not find _KEY_1',
-                              'default/transforms.conf', 5),
-                    call.fail('Have _VAL_1, could not find _KEY_1',
-                              'default/transforms.conf', 8),
-                    call.fail('Have _VAL_1, could not find _KEY_1',
-                              'default/transforms.conf', 11),
-                    call.fail('Have _VAL_1, could not find _KEY_1',
-                              'default/transforms.conf', 14),
-                    call.warn(
-                        'Extra named capture group defined in regex with _KEY_ and _VAL_', 'default/props.conf', 7),
-                    call.warn('Extra named capture group defined in regex with _KEY_ and _VAL_', 'default/transforms.conf', 17)]
-        expected.sort()
-        calls = reporter.mock_calls
-        calls.sort()
-        self.assertListEqual(expected, calls)
+        test_app = "test_data/check_regular_expressions_dynamic_field_names"
+        app = self.get_app(test_app)
+        check_dynamic_field_names_transforms(app, self.reporter)
+        check_dynamic_field_names_props(app, self.reporter)
+        self.assert_mocked_calls(test_app)
 
     def test_check_valid_sedcmd(self):
         """
         Tests that SEDCMD is valid
         """
         from checks.check_regular_expressions import check_valid_regex_sedcmd
-        app = self.get_app("test_data/check_regular_expressions_valid_sedcmd")
-        reporter = Mock()
-        check_valid_regex_sedcmd(app, reporter)
-        expected = [call.fail('Invalid [bad]:SEDCMD-1 of s/regex/replacement', 'default/props.conf', 2),
-                    call.fail('No flags allowed for y/// in SEDCMD',
-                              'default/props.conf', 3),
-                    call.fail(
-                        'For y///, both sides should be the same length', 'default/props.conf', 4),
-                    call.fail('Regex (regex is invalid in SEDCMD-4', 'default/props.conf', 5)]
-        expected.sort()
-        calls = reporter.mock_calls
-        calls.sort()
-        self.assertListEqual(expected, calls)
+        test_app = "test_data/check_regular_expressions_valid_sedcmd"
+        app = self.get_app(test_app)
+        check_valid_regex_sedcmd(app, self.reporter)
+        self.assert_mocked_calls(test_app)
 
     def test_check_valid_regex(self):
         """
@@ -122,98 +100,71 @@ class TestCheckRegularExpressions(unittest.TestCase):
         """
         from checks.check_regular_expressions import check_valid_regex_for_extract
         from checks.check_regular_expressions import check_valid_regex_for_transforms
-        app = self.get_app("test_data/check_regular_expressions_valid_regex")
-        reporter = Mock()
-        check_valid_regex_for_extract(app, reporter)
-        check_valid_regex_for_transforms(app, reporter)
-        expected = [call.fail('Regex (.* is invalid in EXTRACT-1', 'default/props.conf', 2),
-                    call.fail('Regex .*) is invalid in EXTRACT-2',
-                              'default/props.conf', 3),
-                    call.fail(
-                        'Duplicate named groups in (?P<name>.*)(?P<name>.*)', 'default/props.conf', 4),
-                    call.fail(
-                        'Duplicate named groups in (?<name>.*)(?P<name>.*)', 'default/props.conf', 5),
-                    call.fail(
-                        'Duplicate named groups in (?<name>.*)(?<name>.*)', 'default/props.conf', 6),
-                    call.fail('Regex .*) is invalid in REGEX',
-                              'default/transforms.conf', 5),
-                    call.fail(
-                        'Duplicate named groups in (?P<name>.*)(?P<name>.*)', 'default/transforms.conf', 8),
-                    call.fail('Regex (.* is invalid in REGEX',
-                              'default/transforms.conf', 2),
-                    call.fail('Duplicate named groups in (?<name>.*)(?<name>.*)',
-                              'default/transforms.conf', 14),
-                    call.fail('Duplicate named groups in (?<name>.*)(?P<name>.*)', 'default/transforms.conf', 11)]
-        expected.sort()
-        calls = reporter.mock_calls
-        calls.sort()
-        self.assertListEqual(expected, calls)
-
-    def test_props_duplicates(self):
-        from checks.check_regular_expressions import check_duplicate_extract
-        app = self.get_app("test_data/check_regular_expressions_duplicates")
-        reporter = Mock()
-        check_duplicate_extract(app, reporter)
-        expected = [call.warn('Regular expression tA:(?P<_KEY_1>\\w+)\\s*:\\s*(?P<_VAL_1>\\w+) duplicates another extract', 'default/props.conf', 2),
-                    call.warn(
-                        'Regular expression tA:(?P<_KEY_1>\\w+)\\s*:\\s*(?P<_VAL_1>\\w+) duplicates another extract', 'default/props.conf', 3),
-                    call.warn(
-                        'Regular expression tB:(?<_KEY_2>\\w+)\\s*:\\s*(?<_VAL_2>\\w+) duplicates another extract', 'default/props.conf', 5),
-                    call.warn(
-                        'Regular expression tB:(?P<_KEY_2>\\w+)\\s*:\\s*(?P<_VAL_2>\\w+) duplicates another extract', 'default/props.conf', 4),
-                    call.warn(
-                        'Regular expression tC:(?<_KEY_A>\\w+)\\s*:\\s*(?<_VAL_A>\\w+) duplicates another extract', 'default/props.conf', 6),
-                    call.warn('Regular expression tC:(?<_KEY_B>\\w+)\\s*:\\s*(?<_VAL_B>\\w+) duplicates another extract', 'default/props.conf', 7)]
-        expected.sort()
-        calls = reporter.mock_calls
-        calls.sort()
-        self.assertListEqual(expected, calls)
+        test_app = "test_data/check_regular_expressions_valid_regex"
+        app = self.get_app(test_app)
+        check_valid_regex_for_extract(app, self.reporter)
+        check_valid_regex_for_transforms(app, self.reporter)
+        self.assert_mocked_calls(test_app)
 
     def test_transforms_duplicates(self):
+        """
+        Tests for duplicates in transforms.conf, props.conf, and between them.
+        """
         from checks.check_regular_expressions import check_duplicate_transforms_regex
-        app = self.get_app("test_data/check_regular_expressions_duplicates")
-        reporter = Mock()
-        check_duplicate_transforms_regex(app, reporter)
-        expected = [call.warn('Regular expression ([^\\s\\=]+)="([^"]+)" duplicates another REGEX', 'default/transforms.conf', 20),
-                    call.warn(
-                        'Regular expression ([^\\s\\=]+)="([^"]+)" duplicates another REGEX', 'default/transforms.conf', 25),
-                    call.warn(
-                        'Regular expression A:(?P<_KEY_1>\\w+)\\s*:\\s*(?P<_VAL_1>\\w+) duplicates another REGEX', 'default/transforms.conf', 2),
-                    call.warn(
-                        'Regular expression A:(?P<_KEY_1>\\w+)\\s*:\\s*(?P<_VAL_1>\\w+) duplicates another REGEX', 'default/transforms.conf', 5),
-                    call.warn('Regular expression B:(?<_KEY_2>\\w+)\\s*:\\s*(?<_VAL_2>\\w+) duplicates another REGEX',
-                              'default/transforms.conf', 11),
-                    call.warn(
-                        'Regular expression B:(?P<_KEY_2>\\w+)\\s*:\\s*(?P<_VAL_2>\\w+) duplicates another REGEX', 'default/transforms.conf', 8),
-                    call.warn('Regular expression C:(?<_KEY_A>\\w+)\\s*:\\s*(?<_VAL_A>\\w+) duplicates another REGEX',
-                              'default/transforms.conf', 14),
-                    call.warn('Regular expression C:(?<_KEY_B>\\w+)\\s*:\\s*(?<_VAL_B>\\w+) duplicates another REGEX', 'default/transforms.conf', 17)]
-        expected.sort()
-        calls = reporter.mock_calls
-        calls.sort()
-        self.assertListEqual(expected, calls)
-
-    def test_transforms_and_props_duplicates(self):
+        from checks.check_regular_expressions import check_duplicate_extract
         from checks.check_regular_expressions import check_extract_duplicates_transforms
-        app = self.get_app("test_data/check_regular_expressions_duplicates")
-        reporter = Mock()
-        check_extract_duplicates_transforms(app, reporter)
-        expected = [call.warn('[bad]:EXTRACT-7 duplicates transforms seven', 'default/props.conf', 8),
-                    call.warn(
-                        '[bad]:EXTRACT-7 duplicates transforms seven', 'default/props.conf', 8),
-                    call.warn(
-                        '[bad]:EXTRACT-7 duplicates transforms seven', 'default/props.conf', 8),
-                    call.warn(
-                        '[bad]:EXTRACT-7 duplicates transforms seven', 'default/props.conf', 8),
-                    call.warn(
-                        '[bad]:EXTRACT-7 duplicates transforms seven', 'default/props.conf', 8),
-                    call.warn(
-                        '[bad]:EXTRACT-7 duplicates transforms seven', 'default/props.conf', 8),
-                    call.warn('[bad]:EXTRACT-7 duplicates transforms seven', 'default/props.conf', 8)]
-        expected.sort()
-        calls = reporter.mock_calls
-        calls.sort()
-        self.assertListEqual(expected, calls)
+        test_app = "test_data/check_regular_expressions_duplicates"
+        app = self.get_app(test_app)
+        check_duplicate_transforms_regex(app, self.reporter)
+        check_extract_duplicates_transforms(app, self.reporter)
+        check_duplicate_extract(app, self.reporter)
+        self.assert_mocked_calls(test_app)
+
+
+class TestCheckMagicEight(BaseTest):
+    """
+    Tests for the Magic Eight checks.
+    """
+
+    def test_clean(self):
+        """
+        This test checks things in test_data/check_magic_eight_clean, all the
+        props.conf settings are clean for all the checks in check_magic_eight
+        """
+        from checks import check_magic_eight
+        app = self.get_app("test_data/check_magic_eight_clean")
+        for check_name in [c for c in dir(check_magic_eight) if c.startswith("check_")]:
+            getattr(check_magic_eight, check_name)(app, self.reporter)
+        self.assert_clean()
+
+    def test_dirty(self):
+        """
+        Test for bad config for the magic 8
+        """
+        from checks import check_magic_eight
+        app = self.get_app("test_data/check_magic_eight_dirty")
+        for check_name in [c for c in dir(check_magic_eight) if c.startswith("check_")]:
+            getattr(check_magic_eight, check_name)(app, self.reporter)
+        self.assert_mocked_calls("test_data/check_magic_eight_dirty")
+
+    def test_dirty_truncate(self):
+        """
+        Checks for TRUNCATE checks
+        """
+        from checks.check_magic_eight import check_truncate
+        app = self.get_app("test_data/check_magic_eight_truncate")
+        check_truncate(app, self.reporter)
+        self.assert_mocked_calls("test_data/check_magic_eight_truncate")
+
+    def test_max_timestamp_lookahead(self):
+        """
+        Test for MAX_TIMESTAMP_LOOKAHEAD
+        """
+        from checks.check_magic_eight import check_max_timestamp_lookahead
+        test_app = "test_data/check_magic_eight_max_timestamp_lookahead"
+        app = self.get_app(test_app)
+        check_max_timestamp_lookahead(app, self.reporter)
+        self.assert_mocked_calls(test_app)
 
 
 if __name__ == '__main__':
